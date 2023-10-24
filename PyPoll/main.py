d53973e3c2072c2b18562f20c517e03aed803b36
KJMaldonado7 <145682855+KJMaldonado7@users.noreply.github.com>
2023-10-23 22:44:01 -0400

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,36 +4,38 @@ import csv
 csvpath = os.path.join('Resources', 'election_data.csv')
 
 # make a text file for results
-output_file_path = os.path.join('analysis', 'election_data_results.txt')
+txt_path = os.path.join('analysis', 'election_data_results.txt')
 
-# list
-candidates = []
+# dictionary
+candidates = {}
 
 #variables
 total_votes = 0
 winner = ""
 winner_votes = 0
 
-with open(output_file_path, 'w') as output_file:
+with open(txt_path, 'w') as txt_file:
     with open(csvpath, 'r') as file:
         file.readline()
 
         for row in file:
             #add candidates
-            candidates.append(row[2])
+            candidate_name = row[2]
 
             total_votes += 1
-            percent = round(int(row[0]) / total_votes * 100, 2)
+            
+            if candidate_name in candidates:
+                candidates[candidate_name] += 1
+            else: 
+                candidates[candidate_name] = 1
+
+#find winner
+for winner_name, votes in candidates.items():
+    if votes > winner_votes:
+        winner_votes = votes
+        winner = winner_name
     
-    output_file.write("Election Results\n")
-    output_file.write("-------------------\n")
-    output_file.write(f"Total Votes: {total_votes}\n")
-    output_file.write("-------------------\n")
-        for candidate in results.values():
-             output_file.write(f"{candidate['name']}: {candidate['percentage']:.1f}% ({candidate['votes']})\n")
-    output_file.write("-------------------\n")
-    output_file.write(f"Winner: {winner}\n")
-    output_file.write("-------------------\n")
+
 
 # Print the results
 print("Election Results")
@@ -41,9 +43,21 @@ print("-------------------------")
 print(f"Total Votes: {total_votes}")
 print("-------------------------")
 
-for candidate in results.values():
-    print(f"{candidate['name']}: {candidate['percentage']:.1f}% ({candidate['votes']})")
+for candidate, votes in candidates.items():
+    percentage_votes = (votes / total_votes) * 100
+    print(f"{candidate}: {percentage_votes:.1f}% ({votes})")
 
 print("-------------------------")
 print(f"Winner: {winner}")
 print("-------------------------")
+
+txt_file.write("Election Results\n")
+txt_file.write("-------------------\n")
+txt_file.write(f"Total Votes: {total_votes}\n")
+txt_file.write("-------------------\n")
+for candidate, votes in candidates.items():
+    percentage_votes = (votes / total_votes) * 100
+    txt_file.write(f"{candidate}: {percentage:.1f}% ({votes})\n")
+txt_file.write("-------------------\n")
+txt_file.write(f"Winner: {winner}\n")
+txt_file.write("-------------------\n")
